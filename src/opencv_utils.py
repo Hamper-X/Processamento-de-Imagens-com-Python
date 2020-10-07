eb581d5d041e863fd330de66e0c8ad1f6b1cd9e2
mayconbj15 <mayconbj15@gmail.com>
2020-10-06 23:53:59 -0300

    wait key

--- src/opencv_utils.py
+++ src/opencv_utils.py
@@ -17,14 +17,15 @@ def openWindow(window_name, img):
         cv.resizeWindow(window_name, parameters.max_canvas[1], parameters.max_canvas[0])
     else:
         cv.resizeWindow(window_name, img.shape[1], img.shape[0])
-        
+      
     imageShow(window_name, img)
-
+      
 def imageShow(window_name, img):
     if(needResize(img)):
         img = resize(img)
 
     cv.imshow(window_name,img)
+    cv.waitKey()
 
 def needResize(img):
     return img.shape[0] > parameters.max_canvas[0] or img.shape[1] > parameters.max_canvas[1]
