939fd76c736159ddbd8aa0f1e4fd03be99e855b7
mayconbj15 <mayconbj15@gmail.com>
2020-10-13 12:23:00 -0300

    ajuste MUITOS BUGS

--- src/opencv/opencv.py
+++ src/opencv/opencv.py
@@ -12,7 +12,6 @@ window_name = 'image'
 
 global imgPath
 global img
-global originalImg
 
 # Callback functions
 def draw_rectangule(event,x,y,flags,param):
@@ -30,16 +29,15 @@ def draw_rectangule(event,x,y,flags,param):
         
         cv.rectangle(img, (x-parameters.offset,y-parameters.offset), (x+parameters.offset,y+parameters.offset), parameters.color_green, 2, cv.LINE_4)
         
-        opencv_utils.imageShow(window_name, img)
+        opencv_utils.openWindow(window_name, img)
 
 #Core functions
 def abrir_imagem(imagePath):
     global imgPath
     imgPath = imagePath
     global img 
-    global originalImg
     img = opencv_utils.imageRead(imagePath)
-    originalImg = img
+    
     if img is None:
         sys.exit("Could not read the image.")
     
@@ -48,7 +46,7 @@ def abrir_imagem(imagePath):
     cv.setMouseCallback(window_name,draw_rectangule,param=imgPath)
 
 def cortar_imagem():
-    if control.image_checked:
+    if control.image_checked and not control.image_cropped:
         global img
         img = opencv_utils.cropImage(window_name, img)
         opencv_utils.imageShow(window_name, img)
@@ -56,7 +54,7 @@ def cortar_imagem():
 
 def salvar_imagem():
     try:
-        cv.imwrite("../images/processing.png", img)
+        opencv_utils.save_image(img)
     except NameError:
         print('Imagem não carregada')
 
@@ -66,6 +64,11 @@ def zoom(op):
 
 def reset_image():
     try:
-        opencv_utils.openWindow(window_name, originalImg)
+        global img
+        img = opencv_utils.imageRead(imgPath)
+        opencv_utils.openWindow(window_name, img)
+        
+        control.image_cropped = False
+        control.image_checked = False
     except NameError:
         print('Imagem não carregada')
